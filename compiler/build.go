9ba116604455116db7437274dfb7d11b8e78e844
侯锐 <hourui@qianmi.com>
2018-10-16 11:03:08 +0800

    准备进行语法分析的实现

--- compiler/build.go
+++ compiler/build.go
@@ -1,9 +1,61 @@
 package compiler
 
-import "log"
+import (
+	"strings"
+)
+
+type node struct {
+	parent   *node
+	children []*node
+	value    string
+}
 
 func Build(source []byte) (target []byte) {
-	data := string(source)
-	log.Println(data)
+	tokens := lexer(string(source))
+	parser(tokens)
 	return source
+}
+
+// 词法分析
+func lexer(source string) []string {
+	for _, word := range StopWords {
+		source = strings.Replace(source, word, "", -1)
+	}
+	tokens := strings.Split(source, " ")
+
+	tokens1 := make([]string, 0, len(tokens)+2)
+	tokens1 = append(tokens1, ExprStart)
+	for _, token := range tokens {
+		if token != "" {
+			tokens1 = append(tokens1, token)
+		}
+	}
+	tokens1 = append(tokens1, ExprEnd)
+	return tokens1
+}
+
+// 语法分析
+func parser(tokens []string) node {
+	addChild := func(parent *node, child node) {
+
+	}
+	var root node
+	var parent node
+
+	for _, token := range tokens {
+		newNode := node{value: token}
+		addChild(&parent, newNode)
+
+		switch token {
+		case ExprStart:
+		case ExprEnd:
+		case Plus:
+		case Minus:
+		case Multiply:
+		case Divide:
+		default:
+
+		}
+	}
+	return root
 }
---
+++ compiler/constants.go
@@ -0,0 +1,14 @@
+package compiler
+
+var (
+	StopWords = []string{"\n", "\r", "\t"}
+)
+
+const (
+	ExprStart = "("
+	ExprEnd   = ")"
+	Plus      = "+"
+	Minus     = "-"
+	Multiply  = "*"
+	Divide    = "/"
+)
