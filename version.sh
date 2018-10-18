96bba22417a25f9d30fe0fa8922b0d24b5f5b880
侯锐 <hourui@qianmi.com>
2018-10-18 15:25:19 +0800

    优化版本脚本

--- version.sh
+++ version.sh
@@ -3,17 +3,16 @@ read -p "Enter latest version: " version
 read -p "confirm ${version} ? (Y/N): " confirm && [[ ${confirm} == [yY] || ${confirm} == [yY][eE][sS] ]] || exit 1
 echo "latest version -> ${version}"
 
-echo "changing source code"
+echo "Yui -> changing source code"
 versionFile="common/version.go"
-versionFull="\"${version}\""
-sed -i -e "s/\(const Version = \).*/\1$versionFull/" ${versionFile}
+sed -i '' 's/".*"/"'${version}'"/' ${versionFile}
 
-echo "commit source code"
+echo "Yui -> commit source code"
 git add ${versionFile}
-git commit -m"[yui]tag: ${version}"
+git commit -m"[yui -> tag: ${version}]"
 
-echo "create tag and push it"
+echo "Yui -> create tag and push it"
 git tag ${version}
 git push origin ${version}
 
-echo "tag success!"
+echo "Yui -> tag success!"
